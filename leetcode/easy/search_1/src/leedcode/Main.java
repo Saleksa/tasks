9aec3ef4e797e81317e1c7933d941d90cd93716f
Aleksandra <a.a.serova92@mail.ru>
2018-11-13 15:34:10 -0800

    leetcode - easy - improve algorithm

--- leetcode/easy/search_1/src/leedcode/Main.java
+++ leetcode/easy/search_1/src/leedcode/Main.java
@@ -18,13 +18,10 @@ public class Main {
     public static int numJewelsInStones(String J, String S) {
         int count = 0;
 
-        for (int i = 0; i< J.length(); i++) {
-            for (int j = 0; j< S.length(); j++) {
-                if (J.charAt(i) == S.charAt(j)) {
-                    count++;
-                }
-            }
+        for (int i = 0; i< S.length(); i++) {
+            if (J.indexOf(S.charAt(i)) !=  -1) count++;
         }
+        
         return count;
     }
 }
